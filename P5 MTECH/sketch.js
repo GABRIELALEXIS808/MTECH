dab7e25351156956c13c30e6a799cc24ea1f9275
GABRIELALEXIS808 <Gabealexis300@gmail.com>
2026-02-06 13:47:55 -0500

    Files for Work 1

---
+++ P5 MTECH/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,166,600);
+  line(0,0,400,400);
+ ellipse(50, 50, 80, 80);
+  triangle(-20, 25, 8, -30, 36, 25);
+  square(55, 20, 30);
+}
